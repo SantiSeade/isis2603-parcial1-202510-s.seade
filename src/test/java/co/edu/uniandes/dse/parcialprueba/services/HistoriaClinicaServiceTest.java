86a3d8789863c37eba3dfc99fff50bba84c54c9d
FirstName LastName <s.seade@uniandes.edu.co>
2025-03-05 16:45:24 -0500

    Parcial 1

--- src/test/java/co/edu/uniandes/dse/parcialprueba/services/HistoriaClinicaServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/parcialprueba/services/HistoriaClinicaServiceTest.java
@@ -1,4 +1,26 @@
 package co.edu.uniandes.dse.parcialprueba.services;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+
+import co.edu.uniandes.dse.parcialprueba.entities.PacienteEntity;
+import co.edu.uniandes.dse.parcialprueba.exceptions.IllegalOperationException;
+import jakarta.transaction.Transactional;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+@DataJpaTest
+@Transactional
+@Import(HistoriaClinicaService.class)
 public class HistoriaClinicaServiceTest {
 
 }
